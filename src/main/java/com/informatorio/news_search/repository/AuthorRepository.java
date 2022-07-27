d6f2b422f9378ef93b92676a0cc8696e46c7a17b
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-07-27 19:41:35 -0300

    [change]: jpa to paging repository

--- src/main/java/com/informatorio/news_search/repository/AuthorRepository.java
+++ src/main/java/com/informatorio/news_search/repository/AuthorRepository.java
@@ -1,9 +1,9 @@
 package com.informatorio.news_search.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.informatorio.news_search.model.AuthorModel;
 
-public interface AuthorRepository extends JpaRepository<AuthorModel,Long> {
+public interface AuthorRepository extends PagingAndSortingRepository<AuthorModel,Long> {
 
 }
--- src/main/java/com/informatorio/news_search/repository/SourceRepository.java
+++ src/main/java/com/informatorio/news_search/repository/SourceRepository.java
@@ -1,9 +1,9 @@
 package com.informatorio.news_search.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.informatorio.news_search.model.SourceModel;
 
-public interface SourceRepository extends JpaRepository<SourceModel,Long> {
+public interface SourceRepository extends PagingAndSortingRepository<SourceModel,Long> {
     
 }
