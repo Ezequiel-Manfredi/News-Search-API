9d9f9b867b1ed97a75c9d507ea13cab932a47d9e
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-07-27 15:55:14 -0300

    [add]: exception handler for validations

--- src/main/java/com/informatorio/news_search/controller/ExceptionController.java
+++ src/main/java/com/informatorio/news_search/controller/ExceptionController.java
@@ -1,11 +1,17 @@
 package com.informatorio.news_search.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.informatorio.news_search.dto.exception.InvalidFieldExceptionDTO;
 import com.informatorio.news_search.dto.exception.NotFoundExceptionDTO;
 import com.informatorio.news_search.exception.EntityNotFoundException;
 
@@ -18,5 +24,24 @@ public class ExceptionController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public NotFoundExceptionDTO entityNotFound(EntityNotFoundException error){
         return new NotFoundExceptionDTO(error.getName(), error.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler({
+        MethodArgumentNotValidException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<InvalidFieldExceptionDTO> invalidArguments(MethodArgumentNotValidException errors) {
+        return errors
+            .getBindingResult()
+            .getAllErrors()
+            .stream()
+            .map(error -> 
+                new InvalidFieldExceptionDTO(
+                    ((FieldError) error).getField(), 
+                    error.getDefaultMessage()
+                )
+            )
+            .collect(Collectors.toList());
     }
 }
