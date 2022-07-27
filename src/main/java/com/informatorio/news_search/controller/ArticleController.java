c2578062288649b3c9e06a99f36f572fd0ddff1e
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-07-27 19:34:48 -0300

    [add]: validations for request param

--- src/main/java/com/informatorio/news_search/controller/ArticleController.java
+++ src/main/java/com/informatorio/news_search/controller/ArticleController.java
@@ -1,11 +1,12 @@
 package com.informatorio.news_search.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,23 +14,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.informatorio.news_search.dto.article.ArticleDTO;
+import com.informatorio.news_search.dto.article.ArticlePageDTO;
 import com.informatorio.news_search.dto.article.ArticleQueryDTO;
 import com.informatorio.news_search.service.ArticleService;
 
 @RestController
 @RequestMapping(value = "/articles")
+@Validated
 public class ArticleController {
     @Autowired
     ArticleService articleService;
     
     @GetMapping(value = {""})
     @ResponseStatus(HttpStatus.OK)
-    public List<ArticleDTO> allArticles() {
-        return articleService.getAll();
+    public ArticlePageDTO allArticles(
+        @RequestParam(required = false) @Size(min = 3) String query, 
+        @RequestParam @Positive Integer page, 
+        @RequestParam @Positive Integer size
+    ) {
+        if(query != null) {
+            return articleService.getBy(query, page, size);
+        }
+        return articleService.getAll(page, size);
     }
 
     @PostMapping(value = {""})
